cbcf4f27318bbdf19017d96dd252c271154e7a7d
Jarisson Albuquerque <jarissonalbuquerque@gmail.com>
2022-10-06 23:16:11 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+//import { Home } from "./pages/home"
+
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import { CardPokemon } from "./components/CardPokemon";
+import { NavBar } from "./components/NavBar";
+
+export function App() {
+  return (
+    <>
+      <NavBar />
+      <Container maxWidth={false}>
+        <Grid container >
+          <Grid item xs="auto">
+            <CardPokemon />
+          </Grid>
+
+          <Grid item xs={6}>
+            <CardPokemon />
+          </Grid>
+
+          <Grid item xs>
+            <CardPokemon />
+          </Grid>
+
+          {/* <Grid item xs="auto">
+            <CardPokemon />
+          </Grid>
+
+          <Grid item xs={3}>
+            <CardPokemon />
+          </Grid>
+
+          <Grid item xs={3}>
+            <CardPokemon />
+          </Grid> */}
+
+
+        </Grid>
+
+      </Container>
+    </>
+  );
+}
+
+export default App;
