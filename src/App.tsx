f2c59846f56882434bbdb5c378ccef1a95512e2c
Jarisson Albuquerque <jarissonalbuquerque@gmail.com>
2022-10-08 23:16:31 -0300

    first map

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,15 @@
 //import { Home } from "./pages/home"
 
-import Container from "@mui/material/Container";
-import Grid from "@mui/material/Grid";
-import { CardPokemon } from "./components/CardPokemon";
+
 import { NavBar } from "./components/NavBar";
+import { Home } from "./pages/home";
 
 export function App() {
   return (
     <>
       <NavBar />
-      <Container  maxWidth={false}>
-        <Grid container >
-          <Grid item xs={3}>
-            <CardPokemon />
-          </Grid>
-
-          <Grid item xs={3}>
-            <CardPokemon />
-          </Grid>
-
-          <Grid item xs={3}>
-            <CardPokemon />
-          </Grid>
-
-          <Grid item xs={3}>
-            <CardPokemon />
-          </Grid>
-
-          <Grid item xs={3}>
-            <CardPokemon />
-          </Grid>
-
-          <Grid item xs={3}>
-            <CardPokemon />
-          </Grid>
-
-
-        </Grid>
-
-      </Container>
+      <Home />
+     
     </>
   );
 }
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,40 @@
-import React from 'react';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { CardPokemon } from '../components/CardPokemon';
 
-export function Home(){
-    return(
-        <>Home</>
+export function Home() {
+
+    const [pokemons, setPokemons] = useState([])
+
+    useEffect(() => {
+        getPokemons()
+    }, [])
+
+    function getPokemons() {
+        axios.get("https://pokeapi.co/api/v2/pokemon?limit=50&offset=0")
+            .then((res) => setPokemons(res.data.results)).catch((err) => console.log(err))
+    }
+
+    return (
+        <>
+            <Container maxWidth={false}>
+                <Grid container >
+
+                    {pokemons.map((pokemon) => (
+                        <Grid item xs={3}>
+                            <CardPokemon />
+                        </Grid>
+                    ))}
+
+
+
+
+
+                </Grid>
+
+            </Container>
+        </>
     )
 }
